36c8d4f4fa8cd68c65e3f7248e678f65ebf973f6
chandnipatel3121 <chandzzkerai.1231@gmail.com>
2026-05-22 17:36:33 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // Splits third-party libraries into a separate 'vendor' chunk
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    // Raises warning threshold to 1000 kB (1MB) to handle larger vendor chunks smoothly
+    chunkSizeWarningLimit: 1000,
+  },
 })
